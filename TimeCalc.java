71a71f9bd12f6c215e5aba005233144c9822e9f0
Daniel Herschel <danielh2772@gmail.com>
2025-11-05 10:01:21 +0200

    finished TimeCalc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,16 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String currentTime =  args[0];
+        int minutesToAdd = Integer.parseInt(args[1]);
+
+        int hour = Integer.parseInt(currentTime.split(":")[0]);
+        int minute = Integer.parseInt(currentTime.split(":")[1]);
+
+        int totalMinutes = (hour * 60) + minute + minutesToAdd;
+        int totalHours = totalMinutes / 60;
+        int newHours = totalHours % 24;
+        int newMinutes = totalMinutes - (totalHours * 60);
+
+        System.out.printf("%02d:%02d%n", newHours, newMinutes);
     }
 }
