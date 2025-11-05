9929390d2925a3dfcb4bdfa36e42a796b279181e
Daniel Herschel <danielh2772@gmail.com>
2025-11-05 10:35:09 +0200

    finished Cheers

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,24 @@ Great work boaz!
 
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+        String anLetters = "AEFHILMNORSX";
+
+	    String message = args[0];
+        int printCount = Integer.parseInt(args[1]);
+
+        for (char letter : message.toCharArray()) {
+            if (anLetters.indexOf(letter) != -1) {
+                System.out.printf("Give me an %c: %c!%n", letter, letter);
+                continue;
+            }
+
+            System.out.printf("Give me a %c: %c!%n", letter, letter);
         }
+
+        System.out.println("What does that spell?");
+        for (int i = 0; i < printCount; i++) {
+            System.out.println(message + "!!!");
+        }
+    }
 }
